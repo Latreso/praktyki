7a884cb02bf49f4428cb5079ab43ecbd302653dc
Latreso <luki486@interia.pl>
2016-12-05 15:04:05 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,3 @@
 $('.btn-nav').click(function(e){
 	if(!$('body').hasClass('animation')){
 	if($(this).attr('id')=='link-main-page' && $('body').hasClass('podstrony')) {
-		$('body').removeClass('podstrony').addClass('animation');
